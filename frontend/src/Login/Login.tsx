b00725c50984b08ab15fc351b011615bffbba755
yishain11 <yishain11@walla.co.il>
2022-05-30 14:33:46 +0300

    added login page, according the dark theme ui login

---
+++ frontend/src/Login/Login.tsx
@@ -0,0 +1,33 @@
+import "./Login.css";
+
+function Login() {
+  return (
+    <div id="container">
+      <main>
+        <header>
+          <h1>sign in to start agreeing!</h1>
+        </header>
+        <form action="">
+          <p id="username">
+            <label htmlFor="username">USERNAME</label>
+            <input name="username" type="text" />
+          </p>
+          <p id="password">
+            <label htmlFor="password">PASSWORD</label>
+            <input name="password" type="text" />
+          </p>
+          <p id="buttons">
+            <button id="signIn">Sign In</button>
+            <label id="forgot">Forgot Password?</label>
+          </p>
+        </form>
+        <footer>
+          <button id="googleSignin">Continue with Google</button>
+          <button id="discordSignin">Continue with Discord</button>
+        </footer>
+      </main>
+    </div>
+  );
+}
+
+export default Login;
