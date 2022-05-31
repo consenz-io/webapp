dcd33bbc708e4580bed70adc0355f189ccb738ca
yishain11 <yishain11@walla.co.il>
2022-05-31 10:47:16 +0300

    added router

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,17 @@
 import React from "react";
 import "./App.css";
 import Login from "./Login/Login";
+import { Link } from "react-router-dom";
 
 function App() {
-  return <div className="container">{<Login />}</div>;
+  return (
+    <div className="container">
+      <nav>
+        <Link to="/home">Home</Link> | <Link to="/">Login</Link>
+      </nav>
+      <Login />
+    </div>
+  );
 }
 
 export default App;
--- frontend/src/Home/Home.tsx
+++ frontend/src/Home/Home.tsx
@@ -1,6 +1,15 @@
 import "./Home.css";
+import { Link } from "react-router-dom";
+
 function Home() {
-  return <h1>Home page</h1>;
+  return (
+    <div>
+      <nav>
+        <Link to="/home">Home</Link> | <Link to="/">Login</Link>
+      </nav>
+      <h1>Home page</h1>
+    </div>
+  );
 }
 
 export default Home;
