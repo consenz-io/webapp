8bedc3090482af71cfb34a19d5c8bbdb580f7561
yishain11 <yishain11@walla.co.il>
2022-06-06 13:04:31 +0300

    returend basic structure for hasura connection

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,27 @@
 import "./App.css";
-import { Outlet } from "react-router-dom";
+import { useState } from "react";
+import { Link, Outlet } from "react-router-dom";
+import {
+  ApolloClient,
+  ApolloProvider,
+  InMemoryCache,
+  HttpLink,
+} from "@apollo/client";
 
+const createApolloClient = () => {
+  return new ApolloClient({
+    link: new HttpLink({
+      uri: "https://hasura-try-yishain11.hasura.app/v1/graphql",
+      headers: {
+        "x-hasura-admin-secret": ``,
+        "Content-Type": "application/json",
+      },
+    }),
+    cache: new InMemoryCache(),
+  });
+};
 function App() {
+  const [client] = useState(createApolloClient());
   return (
     <div className="container">
       <Outlet />
