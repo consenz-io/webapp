833d793bbbc5a13be5bd7d44ff6f4404d0360c91
yishain11 <yishain11@walla.co.il>
2022-06-06 12:59:48 +0300

    app clean up - before login refactor

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,42 +1,10 @@
-import React, { useState } from "react";
 import "./App.css";
-import Query from "./Query/Query";
-import { Link, Outlet } from "react-router-dom";
-import {
-  ApolloClient,
-  ApolloProvider,
-  InMemoryCache,
-  HttpLink,
-} from "@apollo/client";
+import { Outlet } from "react-router-dom";
 
-const createApolloClient = () => {
-  return new ApolloClient({
-    link: new HttpLink({
-      uri: "https://hasura-try-yishain11.hasura.app/v1/graphql",
-      headers: {
-        "x-hasura-admin-secret": ``,
-        "Content-Type": "application/json",
-      },
-    }),
-    cache: new InMemoryCache(),
-  });
-};
 function App() {
-  const [client] = useState(createApolloClient());
-
   return (
     <div className="container">
-      <nav>
-        <Link to="private">Private</Link> | <Link to="home">Home</Link> |{" "}
-        <Link to="login">Login</Link>
-      </nav>
       <Outlet />
-      <ApolloProvider client={client}>
-        <div>
-          <h3>apolo</h3>
-          <Query />
-        </div>
-      </ApolloProvider>
     </div>
   );
 }
--- frontend/src/Sidebar/Sidebar.tsx
+++ frontend/src/Sidebar/Sidebar.tsx
@@ -1,10 +1,41 @@
 import "./Sidebar.css";
+
+// import Box from "@mui/material/Box";
+// import Drawer from "@mui/material/Drawer";
+// import CssBaseline from "@mui/material/CssBaseline";
+// import AppBar from "@mui/material/AppBar";
+// import Toolbar from "@mui/material/Toolbar";
+// import List from "@mui/material/List";
+// import Typography from "@mui/material/Typography";
+// import Divider from "@mui/material/Divider";
+// import ListItem from "@mui/material/ListItem";
+// import ListItemButton from "@mui/material/ListItemButton";
+// import ListItemIcon from "@mui/material/ListItemIcon";
+// import ListItemText from "@mui/material/ListItemText";
+
+import { styled, useTheme } from "@mui/material/styles";
+import Box from "@mui/material/Box";
+import Drawer from "@mui/material/Drawer";
+import CssBaseline from "@mui/material/CssBaseline";
+import MuiAppBar, { AppBarProps as MuiAppBarProps } from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import List from "@mui/material/List";
+import Typography from "@mui/material/Typography";
+import Divider from "@mui/material/Divider";
+import IconButton from "@mui/material/IconButton";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+
 import {
   SidebarH3,
   SidebarHeaderSection,
   TitleSection,
   TitleText,
 } from "../styles/Sidebar.styles";
+import { useState } from "react";
+import { Link } from "react-router-dom";
 
 function Sidebar() {
   return (
@@ -18,4 +49,152 @@ function Sidebar() {
     </div>
   );
 }
-export default Sidebar;
+
+const drawerWidth = 240;
+const categories = ["finance", "bizdev", "product"]; // placeholder. the true value wil lcome from app state.
+
+// const Main = styled("main", { shouldForwardProp: (prop) => prop !== "open" })<{
+//   open?: boolean;
+// }>(({ theme, open }) => ({
+//   flexGrow: 1,
+//   padding: theme.spacing(3),
+//   transition: theme.transitions.create("margin", {
+//     easing: theme.transitions.easing.sharp,
+//     duration: theme.transitions.duration.leavingScreen,
+//   }),
+//   marginLeft: `-${drawerWidth}px`,
+//   ...(open && {
+//     transition: theme.transitions.create("margin", {
+//       easing: theme.transitions.easing.easeOut,
+//       duration: theme.transitions.duration.enteringScreen,
+//     }),
+//     marginLeft: 0,
+//   }),
+// }));
+
+interface AppBarProps extends MuiAppBarProps {
+  open?: boolean;
+}
+
+const AppBar = styled(MuiAppBar, {
+  shouldForwardProp: (prop) => prop !== "open",
+})<AppBarProps>(({ theme, open }) => ({
+  transition: theme.transitions.create(["margin", "width"], {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.leavingScreen,
+  }),
+  ...(open && {
+    width: `calc(100% - ${drawerWidth}px)`,
+    marginLeft: `${drawerWidth}px`,
+    transition: theme.transitions.create(["margin", "width"], {
+      easing: theme.transitions.easing.easeOut,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+  }),
+}));
+
+const DrawerHeader = styled("div")(({ theme }) => ({
+  display: "flex",
+  alignItems: "center",
+  padding: theme.spacing(0, 1),
+  // necessary for content to be below app bar
+  ...theme.mixins.toolbar,
+  justifyContent: "flex-end",
+}));
+
+function MaterialSidebar() {
+  const theme = useTheme();
+  const [open, setOpen] = useState(false);
+  const handleDrawerOpen = () => {
+    setOpen(true);
+  };
+
+  const handleDrawerClose = () => {
+    setOpen(false);
+  };
+
+  return (
+    // <div id="sidebar">
+    <Box sx={{ display: "flex" }}>
+      <CssBaseline />
+      <AppBar position="fixed" open={open}>
+        <Toolbar>
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            onClick={handleDrawerOpen}
+            edge="start"
+            sx={{ mr: 2, ...(open && { display: "none" }) }}
+          >
+            open
+          </IconButton>
+        </Toolbar>
+      </AppBar>
+      <Drawer
+        sx={{
+          width: drawerWidth,
+          background: "#1e1c21",
+          color: "white",
+          flexShrink: 0,
+          "& .MuiDrawer-paper": {
+            width: drawerWidth,
+            boxSizing: "border-box",
+          },
+        }}
+        variant="persistent"
+        anchor="left"
+        open={open}
+      >
+        <DrawerHeader>
+          <IconButton onClick={handleDrawerClose}>Close</IconButton>
+        </DrawerHeader>
+        <Divider />
+        <h4>links</h4>
+        <List>
+          <ListItem>
+            <Link to="/home">Home</Link>
+          </ListItem>
+          <ListItem>
+            <Link to="/login">Login</Link>
+          </ListItem>
+          ;
+        </List>
+        <Divider />
+        <List>
+          {[
+            "Group Feed",
+            "All Agreements",
+            "Approved Agreements",
+            "Archive",
+          ].map((text, index) => (
+            <ListItem key={text} disablePadding>
+              <ListItemButton>
+                <ListItemIcon>
+                  {/* {index % 2 === 0 ? <InboxIcon /> : <MailIcon />} */}
+                </ListItemIcon>
+                <ListItemText primary={text} />
+              </ListItemButton>
+            </ListItem>
+          ))}
+        </List>
+        <Divider />
+        <h4>Categories</h4>
+        <List>
+          {categories.map((text, index) => (
+            <ListItem key={text} disablePadding>
+              <ListItemButton>
+                <ListItemIcon>icon</ListItemIcon>
+                <ListItemText primary={text} />
+              </ListItemButton>
+            </ListItem>
+          ))}
+        </List>
+        <Divider />
+        <h4>Recent Agreements</h4>
+      </Drawer>
+    </Box>
+  );
+}
+
+// export default Sidebar;
+export default MaterialSidebar;
