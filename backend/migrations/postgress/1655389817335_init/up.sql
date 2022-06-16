5af6dd023d7ffb3d6835f0a057efc131c9d0e074
yishain11 <yishain11@walla.co.il>
2022-06-16 17:31:49 +0300

    added groups schema + migrations (auto gen with hasura console)

--- backend/migrations/postgress/1655389817335_init/up.sql
+++ backend/migrations/postgress/1655389817335_init/up.sql
@@ -1,4 +1,18 @@
 SET check_function_bodies = false;
+CREATE TABLE public.groups (
+    id integer NOT NULL,
+    "createdAt" date NOT NULL,
+    name text NOT NULL,
+    slug text NOT NULL
+);
+CREATE SEQUENCE public.groups_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+ALTER SEQUENCE public.groups_id_seq OWNED BY public.groups.id;
 CREATE TABLE public.users (
     id integer NOT NULL,
     email text NOT NULL,
@@ -12,6 +26,9 @@ CREATE SEQUENCE public.users_id_seq
     NO MAXVALUE
     CACHE 1;
 ALTER SEQUENCE public.users_id_seq OWNED BY public.users.id;
+ALTER TABLE ONLY public.groups ALTER COLUMN id SET DEFAULT nextval('public.groups_id_seq'::regclass);
 ALTER TABLE ONLY public.users ALTER COLUMN id SET DEFAULT nextval('public.users_id_seq'::regclass);
+ALTER TABLE ONLY public.groups
+    ADD CONSTRAINT groups_pkey PRIMARY KEY (id);
 ALTER TABLE ONLY public.users
     ADD CONSTRAINT users_pkey PRIMARY KEY (id);
