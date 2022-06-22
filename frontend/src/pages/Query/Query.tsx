983ab0486cf1ca8e877aed3deda2d960225ec4c8
yishain11 <yishain11@walla.co.il>
2022-06-22 09:56:43 +0300

    docker compuse

--- frontend/src/pages/Query/Query.tsx
+++ frontend/src/pages/Query/Query.tsx
@@ -1,4 +1,4 @@
-import { useMutation, useQuery } from "@apollo/client";
+import { DocumentNode, useMutation, useQuery } from "@apollo/client";
 import { gql } from "@apollo/client";
 import { useEffect } from "react";
 const GET_USERS = gql`
@@ -19,10 +19,29 @@ const INSERT_USER = gql`
     }
   }
 `;
-function createUser(e: any) {
+
+const createUserMut = gql`
+  mutation insert_user($email: String!, $createdAt: Date!) {
+    insert_users_one(object: { email: $email, createdAt: $createdAt }) {
+      email
+      id
+      createdAt
+    }
+  }
+`;
+function CreateUser(e: any) {
+  console.log("start createUser");
   e.preventDefault();
   console.log("create user");
+  const formData = new FormData(e.currentTarget);
+  const email = formData.get("email")?.toString();
+  const createdAt = formData.get("createdAt")?.toString();
+  const { loading, error, data } = useQuery(createUserMut, {
+    variables: { email, createdAt },
+  });
+  return <></>;
 }
+
 const Query = function () {
   const { loading, error, data } = useQuery(GET_USERS);
   if (loading) return <p>Loading...</p>;
@@ -32,6 +51,10 @@ const Query = function () {
   }
   return (
     <div>
+      <nav>
+        <a href="/">home</a>
+        <a href="/login">login</a>
+      </nav>
       <h1>Query</h1>
       <main>
         <h2>data</h2>
@@ -40,13 +63,13 @@ const Query = function () {
             return <div key={user.id}>Email: {user.email}</div>;
           })}
         </main>
-        <form action="">
+        <form action="" onSubmit={CreateUser}>
           <fieldset>
             <label htmlFor="email">Email</label>
             <input type="email" name="email" id="email" />
             <label htmlFor="createdAt">Create At</label>
             <input type="date" name="createdAt" id="createdAt" />
-            <button onClick={createUser}>create user</button>
+            <button type="submit">create user</button>
           </fieldset>
         </form>
       </main>
