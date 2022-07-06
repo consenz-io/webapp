5dcb939a71596f089fe282b41bec210f7618b99f
yishain11 <yishain11@walla.co.il>
2022-07-06 17:49:26 +0300

    added more styles

---
+++ frontend/src/pages/AllAgreements/AllAgreements.style.ts
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const UserAgreements = styled.div`
+  font-family: Lato;
+  font-size: 26px;
+  font-weight: 600;
+  font-stretch: normal;
+  font-style: normal;
+  line-height: 0.85;
+  letter-spacing: normal;
+  text-align: left;
+  color: white;
+  padding: 2rem 1rem;
+`;
