4adb6c675a0e97a056f865bea9334b3bd5cc3d69
Aviran Katz <aviran.katz@gmail.com>
2022-07-13 11:32:03 +0300

    chore: remove group.test column

---
+++ backend/migrations/default/1657701064678_alter_table_core_groups_drop_column_test/down.sql
@@ -0,0 +1,2 @@
+alter table "core"."groups" alter column "test" drop not null;
+alter table "core"."groups" add column "test" int4;
---
+++ backend/migrations/default/1657701064678_alter_table_core_groups_drop_column_test/up.sql
@@ -0,0 +1,1 @@
+alter table "core"."groups" drop column "test" cascade;
