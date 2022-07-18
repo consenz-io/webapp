e0a1e02775b46c08ab2500234a2daeebacf3e265
Aviran Katz <aviran.katz@gmail.com>
2022-07-18 19:36:55 +0300

    chore: extend commitlint.config

--- frontend/commitlint.config.js
+++ frontend/commitlint.config.js
@@ -1,3 +1,17 @@
 module.exports = {
   extends: ['@commitlint/config-conventional'],
+  'type-enum': [
+    'build',
+    'chore',
+    'ci',
+    'docs',
+    'feat',
+    'fix',
+    'merge',
+    'perf',
+    'refactor',
+    'revert',
+    'style',
+    'test',
+  ],
 };
