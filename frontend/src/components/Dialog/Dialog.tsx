b8465bf9c008fe4e1f4ecd580630a43612291ed6
yishain11 <yishain11@walla.co.il>
2022-08-09 17:45:10 +0300

    fix(new cat modal): added style

--- frontend/src/components/Dialog/Dialog.tsx
+++ frontend/src/components/Dialog/Dialog.tsx
@@ -1,10 +1,13 @@
-import { DialogElement, DialogInput } from './style';
+import { DialogMainTitle, DialogElement, DialogInput, DialogButton, CloseButton } from './style';
 import DialogActions from '@mui/material/DialogActions';
-import DialogTitle from '@mui/material/DialogTitle';
+// import DialogTitle from '@mui/material/DialogTitle';
 import { Button } from '@mui/material';
 import DialogProps from './types';
 import { StringBank } from 'strings';
 import { t } from 'i18next';
+// import IconButton from '@mui/material/IconButton';
+import CloseIcon from '@mui/icons-material/Close';
+import './Dialog.css';
 
 export default function DialogEl(props: DialogProps) {
   return (
@@ -18,11 +21,16 @@ export default function DialogEl(props: DialogProps) {
       open={props.openDialogState}
       onClose={props.closeFunction}
     >
-      <DialogTitle>{props.title}</DialogTitle>
+      <div className="titleContainer">
+        <DialogMainTitle>{props.title}</DialogMainTitle>
+        <CloseButton onClick={props.closeFunction}>
+          <CloseIcon />
+        </CloseButton>
+      </div>
       <DialogInput placeholder={t(StringBank.MY_NEW_CATEGORY)} />
       <DialogActions>
         <Button onClick={props.closeFunction}>Cancel</Button>
-        <Button onClick={props.closeFunction}>Create</Button>
+        <DialogButton onClick={props.closeFunction}>Create</DialogButton>
       </DialogActions>
     </DialogElement>
   );
--- frontend/src/components/Dialog/style.ts
+++ frontend/src/components/Dialog/style.ts
@@ -1,5 +1,5 @@
 import styled from 'styled-components';
-import { Dialog, TextField } from '@mui/material';
+import { Dialog, TextField, Button, DialogTitle, IconButton } from '@mui/material';
 
 export const DialogElement = styled(Dialog)`
   && {
@@ -10,7 +10,49 @@ export const DialogInput = styled(TextField)`
   && {
     border-radius: 4px;
     border: solid 1px #565d68;
-    background-color: rgba(0, 0, 0, 0.08);
-    margin: 1rem;
+    /* background-color: rgba(0, 0, 0, 0.08); */
+    margin: 2rem;
+    font-size: 16px;
+    font-family: Lato;
+    color: #adb2b8;
+    height: 42px;
+  }
+`;
+
+export const DialogButton = styled(Button)`
+  && {
+    width: 79px;
+    height: 40px;
+    flex-grow: 0;
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+    gap: 10px;
+    padding: 8px 16px;
+    border-radius: 8px;
+    background-color: #8d54ea;
+  }
+`;
+
+export const DialogMainTitle = styled(DialogTitle)`
+  && {
+    font-family: Lato;
+    font-size: 1.2rem;
+    font-weight: 600;
+    font-stretch: normal;
+    font-style: normal;
+    line-height: 1.09;
+    letter-spacing: normal;
+    text-align: left;
+    color: white;
+    display: inline;
+  }
+`;
+
+export const CloseButton = styled(IconButton)`
+  && {
+    width: 50px;
+    height: 50px;
   }
 `;
