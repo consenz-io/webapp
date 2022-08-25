0c10661e40647c730305bbd4049ec5acceb48f3e
yishain11 <yishain11@walla.co.il>
2022-08-25 19:33:25 +0300

    feat(added icon and styles): added X icon and imprived padding and colors

--- frontend/src/components/Appbar/Appbar.tsx
+++ frontend/src/components/Appbar/Appbar.tsx
@@ -1,11 +1,27 @@
 import { FC, useState } from 'react';
 import { AppbarProps } from './types';
-import SummarizeOutlinedIcon from '@mui/icons-material/SummarizeOutlined';
-import { IconButton, Stack, Typography } from '@mui/material';
+import { IconButton, Stack, StepLabel, SvgIcon, Typography } from '@mui/material';
 import Stepper from '@mui/material/Stepper';
 import Step from '@mui/material/Step';
 import StepButton from '@mui/material/StepButton';
-import CloseIcon from '@mui/icons-material/Close';
+import { ReactComponent as DocLogo } from 'assets/icons/document.svg';
+import { ReactComponent as XLogo } from 'assets/icons/x-circle.svg';
+import styled from 'styled-components';
+
+const AppbarContainer = styled(Stack)`
+  && {
+    align-items: center;
+    border-bottom: solid 1px #595f68;
+    margin: 0.35rem -1rem;
+    padding: 0 1rem;
+  }
+`;
+
+const XIconWrapper = styled(SvgIcon)`
+  svg path {
+    fill: #adb2b8;
+  }
+`;
 
 const Appbar: FC<AppbarProps> = (props) => {
   const steps = ['Rationale', 'Sections', 'Rules'];
@@ -20,31 +36,44 @@ const Appbar: FC<AppbarProps> = (props) => {
   };
 
   return (
-    <Stack
-      direction="row"
-      alignItems="center"
-      justifyContent="space-between"
-      sx={{ borderBottom: 'solid 1px #595f68', width: '85.3vw' }}
-    >
-      <Stack direction="row" gap="0.5rem">
-        <SummarizeOutlinedIcon />
+    <AppbarContainer direction="row" alignItems="center" justifyContent="space-between">
+      <Stack direction="row" gap="0.5rem" sx={{ paddingBottom: '1rem' }}>
+        <DocLogo />
         <Typography>{props.agreementName}</Typography>
       </Stack>
-      <Stack direction="row">
+      <Stack direction="row" sx={{ paddingBottom: '1rem' }}>
         <Stepper nonLinear activeStep={activeStep}>
           {steps.map((label, index) => (
-            <Step key={label} completed={completed[index]}>
+            <Step key={label} completed={completed[index]} sx={{ padding: '0 1rem' }}>
               <StepButton color="inherit" onClick={handleStep(index)}>
-                {label}
+                <StepLabel
+                  sx={{
+                    '& .css-dsfbi5-MuiSvgIcon-root-MuiStepIcon-root': {
+                      color: '#595f68',
+                    },
+                    '& .css-dsfbi5-MuiSvgIcon-root-MuiStepIcon-root.Mui-active': {
+                      color: '#8d54ea',
+                    },
+                  }}
+                >
+                  {label}
+                </StepLabel>
               </StepButton>
             </Step>
           ))}
         </Stepper>
       </Stack>
-      <IconButton onClick={props.closeFn}>
-        <CloseIcon />
+      <IconButton
+        sx={{
+          position: 'realtive',
+          bottom: '.5rem',
+        }}
+      >
+        <XIconWrapper onClick={props.closeFn}>
+          <XLogo />
+        </XIconWrapper>
       </IconButton>
-    </Stack>
+    </AppbarContainer>
   );
 };
 
