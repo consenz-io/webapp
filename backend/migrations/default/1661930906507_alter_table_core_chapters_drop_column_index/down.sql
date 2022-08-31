c3d55730b83eeb49e7fc2cf838974965afd1e308
Aviran Katz <aviran.katz@gmail.com>
2022-08-31 10:30:51 +0300

    refactor: removed redundant migration

--- backend/migrations/default/1661930906507_alter_table_core_chapters_drop_column_index/down.sql
+++
@@ -1,2 +0,0 @@
-alter table "core"."chapters" alter column "index" drop not null;
-alter table "core"."chapters" add column "index" int4;
--- backend/migrations/default/1661930906507_alter_table_core_chapters_drop_column_index/up.sql
+++
@@ -1,1 +0,0 @@
-alter table "core"."chapters" drop column if exists "index" cascade;
