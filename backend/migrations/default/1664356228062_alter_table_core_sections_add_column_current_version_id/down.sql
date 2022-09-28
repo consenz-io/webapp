f49b6acde8fb2d1f9fca1982828ed89ec798b49b
yishain11 <yishain11@walla.co.il>
2022-09-28 12:12:47 +0300

    feat(currentVersion): added section curent version

---
+++ backend/migrations/default/1664356228062_alter_table_core_sections_add_column_current_version_id/down.sql
@@ -0,0 +1,4 @@
+-- Could not auto-generate a down migration.
+-- Please write an appropriate down migration for the SQL below:
+-- alter table "core"."sections" add column "current_version_id" integer
+--  null unique;
