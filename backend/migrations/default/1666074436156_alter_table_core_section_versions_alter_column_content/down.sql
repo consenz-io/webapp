83df9343b822aeec5e7dc8d90c1a30a037c7738c
Aviran Katz <aviran.katz@gmail.com>
2022-10-18 09:28:02 +0300

    fix: revert content type

---
+++ backend/migrations/default/1666074436156_alter_table_core_section_versions_alter_column_content/down.sql
@@ -0,0 +1,3 @@
+-- Could not auto-generate a down migration.
+-- Please write an appropriate down migration for the SQL below:
+ALTER TABLE "core"."section_versions" ALTER COLUMN "content" TYPE text;
