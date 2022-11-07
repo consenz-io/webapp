90354e31aa8986c6e7dcce225150d6c0032e2e47
Aviran Katz <aviran.katz@gmail.com>
2022-11-07 16:53:35 +0200

    feat: users_public view, full_name column

---
+++ backend/migrations/default/1667832432602_run_sql_migration/down.sql
@@ -0,0 +1,6 @@
+-- Could not auto-generate a down migration.
+-- Please write an appropriate down migration for the SQL below:
+-- CREATE OR REPLACE VIEW core.users_public AS
+--   SELECT id, full_name
+--     FROM core.users;
+drop view core.users_public;
