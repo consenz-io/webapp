541263ca7b4d327b85817e5d6fa8106351d5d4f3
Joel Rothschild <joel.rothschild@jacksonriver.com>
2022-11-08 11:12:38 -0500

    chore: refactor agreement_consensus_average function

---
+++ backend/migrations/default/1667923795914_alter_function_agreement_consensus_average/up.sql
@@ -0,0 +1,11 @@
+CREATE OR REPLACE FUNCTION core.agreement_consensus_average(agreements_row core.agreements)
+    RETURNS NUMERIC
+    LANGUAGE sql
+    STABLE
+AS $$
+    SELECT AVG(sv.consensus)::NUMERIC(10,2)
+    FROM core.chapters c 
+    LEFT JOIN core.sections s ON s.chapter_id=c.id 
+    LEFT JOIN core.section_versions sv ON sv.id=s.current_version_id
+    WHERE c.agreement_id=agreements_row.id AND sv.consensus IS NOT NULL
+$$;
