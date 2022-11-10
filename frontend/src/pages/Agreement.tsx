6ba53b9853131d22ae2db42ace60f3457b4d0a39
yishain11 <yishain11@walla.co.il>
2022-11-10 14:28:35 +0200

    fix(chapter): chpater id added

--- frontend/src/pages/Agreement.tsx
+++ frontend/src/pages/Agreement.tsx
@@ -97,7 +97,7 @@ const Agreement: FC = () => {
         <Typography>{agreement?.rationale}</Typography>
         <Box />
         <Stack>
-          {agreement?.chapters?.map((chapter: Chapter) => (
+          {agreement?.chapters?.map((chapter: Chapter, chapterIndex: number) => (
             <Accordion
               TransitionProps={{ unmountOnExit: true }}
               defaultExpanded
@@ -145,7 +145,7 @@ const Agreement: FC = () => {
                     <IconButton
                       onClick={() => {
                         setCurrentChapterId(chapter.id);
-                        setCurrentSectionIndex(chapter?.sections[0].index);
+                        setCurrentSectionIndex(chapter?.sections[chapterIndex]?.index);
                         setIsTextPopupOpen(true);
                       }}
                       sx={{
