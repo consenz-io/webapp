23f7a58d9c1261d940d95ca1bf9ab5080ddc00e5
yishain11 <yishain11@walla.co.il>
2022-11-17 15:40:35 +0200

    fix(bug): working on insert error 1

--- backend/migrations/default/1668093456409_run_sql_migration/down.sql
+++ backend/migrations/default/1668093456409_run_sql_migration/down.sql
@@ -1,2 +1,3 @@
 drop trigger if exists run_reorder_indexes on core.sections;
 drop function if exists core.reorder_indexes;
+drop function if exists core.chapter_to_agreement;
--- backend/migrations/default/1668093456409_run_sql_migration/up.sql
+++ backend/migrations/default/1668093456409_run_sql_migration/up.sql
@@ -1,14 +1,34 @@
+create or replace function core.chapter_to_agreement(chapter_id_new int)
+    returns int as $$
+    declare
+        agreement_id_found int;
+    begin
+        select agreement_id from core.sections s 
+        left join core.chapters c on chapter_id_new=c.id 
+        left join core.agreements a on c.agreement_id=a.id 
+        limit(1)
+        into agreement_id_found;
+        return agreement_id_found;
+    end;
+    $$ language plpgsql;
+
 create or replace function core.reorder_indexes()
 returns trigger as $$
     DECLARE
         rec RECORD;
+        inserted_chapter_id integer;
+        inserted_agreement_id integer;
+        row_agreement_id integer;
     begin
+        inserted_chapter_id=NEW.chapter_id;
+        select * from core.chapter_to_agreement(inserted_chapter_id) into inserted_agreement_id;
         FOR rec IN
-            select * from core.sections order by index
+            select * from core.sections
         LOOP
+        select * from core.chapter_to_agreement(rec.chapter.id) into row_agreement_id;
         update core.sections
         set index = rec.index +1
-        where id = rec.id and rec.index >= NEW.index and id != NEW.id;
+        where id = rec.id and rec.index >= NEW.index and id != NEW.id and row_agreement_id=inserted_agreement_id;
     END LOOP;
     RETURN NEW;
     end;
