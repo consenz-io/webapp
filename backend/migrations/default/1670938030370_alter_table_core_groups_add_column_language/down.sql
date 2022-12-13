34d4b42841ac696764d51fde5efa5190f6878867
yishain11 <yishain11@walla.co.il>
2022-12-13 15:28:19 +0200

    feat(lan): added lan field to group table

---
+++ backend/migrations/default/1670938030370_alter_table_core_groups_add_column_language/down.sql
@@ -0,0 +1,4 @@
+-- Could not auto-generate a down migration.
+-- Please write an appropriate down migration for the SQL below:
+-- alter table "core"."groups" add column "language" Text
+--  null default 'en';
